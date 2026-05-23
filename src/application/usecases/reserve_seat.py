ec593b5b1ae89e97ff87965bdd4d4bfbf99e4198
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-05-23 11:48:06 -0600

    feature: reserva temporal de asientos HU1

---
+++ src/application/usecases/reserve_seat.py
@@ -0,0 +1,34 @@
+import uuid
+
+from datetime import datetime, timedelta, timezone
+
+from domain import SeatRepository, OrderRepository, TaskScheduler
+
+
+class ReserveSeatUseCase:
+    def __init__(self, seat_repository: SeatRepository,
+                 order_repository: OrderRepository, scheduler: TaskScheduler) -> None:
+        self.seat_repository = seat_repository
+        self.order_repository = order_repository
+        self.scheduler = scheduler
+
+    def execute(self, seat_id: int) -> dict:
+        seat = self.seat_repository.get_seat_for_update(seat_id)
+
+        if not seat:
+            raise ValueError(f'El asiento con id: {seat_id} no existe')
+
+        seat.reserve()
+        self.seat_repository.save(seat)
+
+        order = self.order_repository.create_pending_order(seat_id)
+        
+        expires_at = datetime.now(timezone.utc) + timedelta(minutes=8)
+        job_id = f'expire_seat_{seat_id}_order_{order.id}'
+
+        self.scheduler.schedule_expiration(job_id, expires_at, seat_id, order.id)
+
+        return {
+            'reservation_id': str(uuid.uuid4()),
+            'expires_at': expires_at.isoformat()
+        }
