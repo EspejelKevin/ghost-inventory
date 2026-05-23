f19ec63cc0e2dfa30341c7f65a49cfc9760dcab7
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-05-23 13:23:09 -0600

    feature: inyeccion de dependencias

---
+++ src/container.py
@@ -0,0 +1,37 @@
+from dependency_injector import containers, providers
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.redis import RedisJobStore
+
+from infrastructure import PostgresSeatRepository, PostgresOrderRepository, APSchedulerAdapter, get_db_session
+from application import ReserveSeatUseCase
+
+
+class Container(containers.DeclarativeContainer):
+    db_session = providers.Resource(get_db_session)
+
+    scheduler = providers.Singleton(
+        AsyncIOScheduler,
+        jobstores={'default': RedisJobStore(host='localhost', port=6379, db=0)}
+    )
+
+    seat_repository = providers.Factory(
+        PostgresSeatRepository,
+        session=db_session
+    )
+
+    order_repository = providers.Factory(
+        PostgresOrderRepository,
+        session=db_session
+    )
+
+    task_scheduler = providers.Factory(
+        APSchedulerAdapter,
+        scheduler=scheduler
+    )
+
+    reserve_seat_usecase = providers.Factory(
+        ReserveSeatUseCase,
+        seat_repository=seat_repository,
+        order_repository=order_repository,
+        scheduler=scheduler
+    )
