571d69e669aaa0160f7ac9139bda046605fd6ffd
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-05-25 21:39:58 -0600

    feature: creacion del archivo main.py

---
+++ src/main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI, HTTPException, status, Depends
+from dependency_injector.wiring import inject, Provide
+from pydantic import BaseModel
+
+import sys
+
+from src.container import Container
+from src.application import ReserveSeatUseCase
+from src.infrastructure import SessionLocal, SeatModel
+
+
+container = Container()
+
+
+async def startup_event():
+    scheduler_adapter = container.task_scheduler()
+    scheduler_adapter.scheduler.start()
+    print('APScheduler iniciado y conectado a Redis')
+
+async def shutdown_event():
+    scheduler_adapter = container.task_scheduler()
+    scheduler_adapter.scheduler.shutdown()
+
+    
+app = FastAPI(title='Ghost Inventory API', 
+              on_startup=[startup_event], on_shutdown=[shutdown_event])
+
+
+@app.post('/api/v1/seats/{seat_id}/reserve', status_code=status.HTTP_201_CREATED)
+@inject
+def reserve_seat_endpoint(
+    seat_id: int,
+    usecase: ReserveSeatUseCase = Depends(Provide[Container.reserve_seat_usecase])
+):
+    try:
+        result = usecase.execute(seat_id)
+        return {
+            'status': 'success',
+            'message': 'asiento reservado exitosamente.',
+            'data': result
+        }
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Error interno del servidor: {str(e)}')
+
+
+class SeatCreate(BaseModel):
+    id: int
+
+
+@app.post('/dev/seats', status_code=status.HTTP_201_CREATED, tags=['development'])
+def create_test_seat(seat: SeatCreate):
+    db = SessionLocal()
+    try:
+        db_seat = db.query(SeatModel).filter(SeatModel.id == seat.id).first()
+        if db_seat:
+            raise HTTPException(status_code=409, detail='El asiento ya existe')
+        
+        new_seat = SeatModel(id=seat.id, status='DISPONIBLE')
+        db.add(new_seat)
+        db.commit()
+        return {'message': f'Asiento {seat.id} creado como DISPONIBLE'}
+    finally:
+        db.close()
+
+
+container.wire(modules=[sys.modules[__name__]])
